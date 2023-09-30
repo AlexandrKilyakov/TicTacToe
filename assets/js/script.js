5a95d03ba4e7ab0222640fa16c4efb3e51b21c90
Alexander Kilyakov <kalataaleksandr@gmail.com>
2023-10-01 01:19:13 +0300

    Feat: Готова логика проверки победы по горизонтали и вертикали

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,22 +8,39 @@ const game = document.querySelector(selectors.game);
 const btnRestart = document.querySelector(selectors.restart);
 
 const size = 3;
+const stopGame = "finish";
 const changeUnit = {
   x: "o",
   o: "x",
 };
+const stepUnits = {
+  x: getArraySize(),
+  o: getArraySize(),
+};
+
+let victory = false;
 
 function createGamePoints() {
-  const n = size ** 2;
-
   game.style.setProperty("--width", `${100 / size}%`);
   game.innerHTML = "";
 
-  for (let i = 0; i < n; i++) {
-    game.innerHTML += `<div class="point"></div>`;
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      game.innerHTML += `<div class="point" data-y="${y}" data-x="${x}"></div>`;
+    }
   }
 
   setUnit("o");
+}
+
+function getArraySize() {
+  const array = [];
+
+  for (let i = 0; i < size; i++) {
+    array.push([]);
+  }
+
+  return array;
 }
 
 function getUnit() {
@@ -32,6 +49,36 @@ function getUnit() {
 
 function setUnit(unit) {
   game.dataset.unit = changeUnit[unit];
+}
+
+function elementMatches(i, item, arr) {
+  i++;
+
+  // Если дошли до конца массива
+  if (arr.length <= i) {
+    return true;
+  }
+
+  // Если пусто
+  if (!arr[i].length) {
+    return false;
+  }
+
+  return arr[i].includes(item) && elementMatches(i, item, arr);
+}
+
+function victoryUnit(unit) {
+  // Заполнена ли строка
+  for (element of stepUnits[unit]) {
+    if (element.length == size) {
+      return true;
+    }
+  }
+
+  // Есть ли совпадения
+  return stepUnits[unit][0].some((item) =>
+    elementMatches(0, item, stepUnits[unit])
+  );
 }
 
 function restartGame() {
@@ -48,6 +95,16 @@ function clickPoint(target) {
   const unit = getUnit();
 
   point.classList.add(`here-${unit}`);
+  // console.log(stepUnits);
+  stepUnits[unit][~~point.dataset.y].push(~~point.dataset.x);
+  victory = victoryUnit(unit);
+
+  console.log(victory);
+
+  if (victory) {
+    return;
+  }
+
   setUnit(unit);
 }
 
@@ -61,3 +118,27 @@ function start() {
 }
 
 start();
+
+// По горизонтали: Разница 1, уравень один
+// 1|1 1|2 1|3
+// 2|4 2|5 2|6
+// 3|7 3|8 3|9
+
+// По вертикали: Разница 3, уравень возрастает на 1
+// 1|1 2|4 3|7
+// 1|2 2|5 3|8
+// 1|3 2|6 3|9
+
+// По диагонали:
+// 1|1 2|5 3|9 Разница 4, уравень возрастает на 1
+// 1|3 2|5 3|7 Разница 2, уравень возрастает на 1
+
+//  Что искать?
+// 1. Заполнены ли 3 ячейки одного ряда
+// 2. Заполнены ли 3 ячейки разного ряда
+// 3. Найти в них закономерность (одинаковую разность)
+
+// Победа
+// 1. Если длина ряда равна длине размера 3 / 3
+// 2. Если во всех столбцах есть совпадение
+// 3. Если есть по возрастанию или убыванию
