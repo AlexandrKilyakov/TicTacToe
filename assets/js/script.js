e0553e1bf8f39a5c10afa308301d1162b590f96c
Alexander Kilyakov <kalataaleksandr@gmail.com>
2023-09-30 21:14:36 +0300

    Feat: Добавил функцию создания игрового поля. Добавил функцию рестарта

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const selectors = {
+  game: "[data-game]",
+  restart: ".restart",
+};
+
+const game = document.querySelector(selectors.game);
+const btnRestart = document.querySelector(selectors.restart);
+
+const size = 3;
+
+function createGamePoints() {
+  const n = size ** 2;
+
+  game.style.setProperty("--size", size);
+  game.innerHTML = "";
+
+  for (let i = 0; i < n; i++) {
+    game.innerHTML += `<div class="place"></div>`;
+  }
+
+  game.dataset.unit = "x";
+}
+
+function restartGame() {
+  createGamePoints();
+}
+
+function start() {
+  createGamePoints();
+
+  btnRestart.addEventListener("click", restartGame);
+}
+
+start();
