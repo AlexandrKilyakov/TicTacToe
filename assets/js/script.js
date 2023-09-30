3185f8a8393b7245b9436543b9c5a107b2edd8a5
Alexander Kilyakov <kalataaleksandr@gmail.com>
2023-09-30 23:29:04 +0300

    Feat: Готова функция, которая ставит крестик или нолик в поле

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 const selectors = {
   game: "[data-game]",
   restart: ".restart",
+  point: ".point",
 };
 
 const game = document.querySelector(selectors.game);
 const btnRestart = document.querySelector(selectors.restart);
 
 const size = 3;
+const changeUnit = {
+  x: "o",
+  o: "x",
+};
 
 function createGamePoints() {
   const n = size ** 2;
@@ -15,20 +20,44 @@ function createGamePoints() {
   game.innerHTML = "";
 
   for (let i = 0; i < n; i++) {
-    game.innerHTML += `<div class="place"></div>`;
+    game.innerHTML += `<div class="point"></div>`;
   }
 
-  game.dataset.unit = "x";
+  setUnit("o");
+}
+
+function getUnit() {
+  return game.dataset.unit;
+}
+
+function setUnit(unit) {
+  game.dataset.unit = changeUnit[unit];
 }
 
 function restartGame() {
   createGamePoints();
 }
 
+function clickPoint(target) {
+  const point = target.closest(selectors.point);
+
+  if (!point) {
+    return;
+  }
+
+  const unit = getUnit();
+
+  point.classList.add(`here-${unit}`);
+  setUnit(unit);
+}
+
 function start() {
   createGamePoints();
 
   btnRestart.addEventListener("click", restartGame);
+  game.addEventListener("click", ({ target }) => {
+    clickPoint(target);
+  });
 }
 
 start();
